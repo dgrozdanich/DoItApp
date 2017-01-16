1ab06a7df5cc8ca6ba734b01c6d5fc68fc6f2bba
Dylan Grozdanich <dgrozdanich@gmail.com>
2017-01-15 16:37:24 -0800

    Added class, tableview and got first page of todo app working.

---
+++ DoItApp/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoItApp
+//
+//  Created by Dylan Grozdanich on 1/15/17.
+//  Copyright © 2017 Dylan Grozdanich. All rights reserved.
+//
+
+import Foundation
+
+
+class Task {
+    var name = ""
+    var important = false
+}
